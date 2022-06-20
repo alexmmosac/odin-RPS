01e8ade831b841739f07a22b14d61139ebba2644
alexmmosac <alexmmosac@outlook.com>
2022-06-19 20:21:30 -0400

    Done with core logic; need to add function on button for end game restart.

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-//TODO: Add Player and computer images to middle. 
-//TODO: Add Player and computer Counters. 
+//DONE: Add Player and computer images to middle. 
+//DONE: Add Player and computer Counters. 
 //TODO: Make function to display reset button if Rounds played hits 5. Then Reset all counters. 
-
-
-
+document.getElementById("end_game").style.display= "none";
 function player_selection(){
-if(count == 5){
-//TODO: add computer and player counters. 
-}
-else{
 const r = document.getElementById("rock");
 const p = document.getElementById("paper");
 const s = document.getElementById("scissors");
@@ -16,10 +10,9 @@ r.addEventListener('click', function(){ assignVal(1); });
 p.addEventListener('click', function(){ assignVal(2); });
 s.addEventListener('click', function(){ assignVal(3); });
 }
-}
 
 function assignVal(num){
-    var x = num; 
+    var x = num;
     computer_selection(x);
 
 }
@@ -31,51 +24,103 @@ function computer_selection(player_pick){
 
 
 function game_engine(player, computer){
+    set_versus(player, computer);
     var winner_text = document.querySelector(".Winner");
-   // var images = document.querySelector(".image_w_l");
     var rounds_played = document.querySelector(".round_counter");
-  
-    if(player == computer){         //TIE EVENt
-       // document.write("TIE!");
+    var player_counter = document.querySelector(".player_count");
+    var computer_counter = document.querySelector(".computer_count");
+    if(player == computer){         //TIE EVENT No increment on Players scores.
        winner_text.innerHTML = "It's a TIE!";
-       count += 1;
-       rounds_played.innerHTML = "Rounds Played:" + count;
+       rounds_played.innerHTML = parseInt(rounds_played.innerHTML)+1;
     }
-    else if(player == 1 && computer == 3){  //ROCK V. Scissors
-        //document.write("Player wins!");
-        
+    else if(player == 1 && computer == 3){  //ROCK V. Scissors  Player win Increment Player score
         winner_text.innerHTML = "Player Wins!";
-        count += 1;
-        rounds_played.innerHTML = "Rounds Played:" + count;
-    
+        player_counter.innerHTML = parseInt(rounds_played.innerHTML)+1;
+        rounds_played.innerHTML = parseInt(rounds_played.innerHTML)+1;
     }
     else if(player == 3 && computer == 1){ // Scissors V. Rock
-       // document.write("Computer wins!");
        winner_text.innerHTML = "Computer Wins!";
-       count += 1;
-       rounds_played.innerHTML = "Rounds Played:" + count;
+       computer_counter.innerHTML = parseInt(computer_counter.innerHTML)+1;
+       rounds_played.innerHTML = parseInt(rounds_played.innerHTML)+1;
     }
-    else if(player > computer ){        // Natural flow of Greater then 
-        //document.write("Player wins!");
+    else if(player > computer ){        // Natural flow of Greater then
         winner_text.innerHTML = "Player Wins!";
-        count += 1;
-        rounds_played.innerHTML = "Rounds Played:" + count;
+        player_counter.innerHTML = parseInt(rounds_played.innerHTML)+1;
+        rounds_played.innerHTML = parseInt(rounds_played.innerHTML)+1;
     }
     else if(player < computer){         // Natural flow of Greater then
-        //document.write("Computer wins");
-        
         winner_text.innerHTML = "Computer Wins!";
-        count += 1;
-        rounds_played.innerHTML = "Rounds Played:" + count;
+        computer_counter.innerHTML = parseInt(computer_counter.innerHTML)+1;
+        rounds_played.innerHTML = parseInt(rounds_played.innerHTML)+1;
     }
     else{
-        //document.write("NO CLEAR WINNER!");
         winner_text.innerHTML = "Error_02: No clear winner picked;";
+    }
+    var rounds_played = document.querySelector(".round_counter");
+    var count = parseInt(rounds_played.innerHTML);
+    var player_counter = document.querySelector(".player_count");
+    var player_score = parseInt(player_counter.innerHTML);
+    var computer_counter = document.querySelector(".computer_count");
+    var computer_score = parseInt(computer_counter.innerHTML);
+
+    if(count == 5){
+        document.getElementById("Game_content").style.display = "none";
+        var end_winner = document.querySelector(".End_Winner");
+        if(computer_score > player_score){
+            end_winner.innerHTML = "Computer Wins";
+            document.getElementById("end_game").style.display= "block";
+         /*
+            const play_again = document.getElementById("button");
+            play_again.addEventListener('click', function(){ replay(); });
+        */
+        }
+        else if(computer_score < player_score){
+            end_winner.innerHTML = "Player Wins";
+            document.getElementById("end_game").style.display= "block";
+          /*
+            const play_again = document.getElementById("button");
+            play_again.addEventListener('click', function(){ replay(); });
+        */
+        }
     }
 }
 
 
-let count = 0;
+function set_versus(player_pick, computer_pick){
+    if(player_pick == 1){
+        document.getElementById("player_i").src="images/Rock.png";
+    }
+    else if(player_pick == 2){
+        document.getElementById("player_i").src="images/Paper.png";
+    }
+    else if(player_pick == 3){
+        document.getElementById("player_i").src="images/Scissors.png";
+    }
+    else{
+        console.log("ERROR_03: Player_pick Uncertain");
+    }
+
+    if(computer_pick == 1){
+        document.getElementById("computer_i").src="images/Rock.png";
+    }
+    else if(computer_pick == 2){
+        document.getElementById("computer_i").src="images/Paper.png";
+    }
+    else if(computer_pick == 3){
+        document.getElementById("computer_i").src="images/Scissors.png";
+    }
+    else{
+        console.log("ERROR_03: Player_pick Uncertain");
+    }
+
+}
+
+
+
+
+
+
+
 player_selection();
 
 
